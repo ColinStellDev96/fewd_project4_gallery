9f9dc072ab727ca6364b1480c18eeac9c7c659a3
Colin Stell <ColinStellDev96@users.noreply.github.com>
2018-06-14 09:53:13 -0600

    finalized JS

--- js/main.js
+++ js/main.js
@@ -7,10 +7,10 @@ $('#filter').on('keyup', function (event) {
     var srchStr = $(this).val().toLowerCase();
     console.log(srchStr);
     $('.image a').each(function(){
-        if($(this).attr('data-title').toLowerCase().indexOf(srchStr) == -1) {
+        if($(this).attr('data-title').toLowerCase().indexOf(srchStr) == -1 && $(this).attr('data-alt').toLowerCase().indexOf(srchStr) == -1 )  {
             $(this).hide(1000);
         } else {
-            $(this).show();
+            $(this).show(1000);
         }
     });
 });
